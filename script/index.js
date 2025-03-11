790210384ad1e7d2a56aabe840d0cccac6ae8eac
mdmhrz <razufreelence@gmail.com>
2025-03-12 04:03:38 +0600

    Category Button API done

--- script/index.js
+++ script/index.js
@@ -1,1 +1,29 @@
-console.log('hello java script!');
+
+
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+
+const loadCategory = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then(res => res.json())
+        .then(data => displayCategory(data.categories));
+}
+
+function displayCategory(data) {
+    for (const cat of data) {
+        console.log(cat.category)
+
+        const categoryBtnContainer = document.getElementById('category-btn-container');
+
+        const catBtn = document.createElement('div')
+        catBtn.innerHTML = `
+            <button class="btn btn-sm">${cat.category}</button>
+        `
+        categoryBtnContainer.append(catBtn)
+    }
+
+}
+
+loadCategory()
